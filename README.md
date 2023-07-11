e0ae6f50804fe1ecf3fd551d3ebf9f3c50cc5741
Rodrigo Tufiño <rtufino@ups.edu.ec>
2023-07-11 12:51:13 -0500

    Get Token

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # drf-crud
 CRUD con Django Rest Framework
 
+## Tutorial Base:
 https://www.youtube.com/watch?v=GE0Q8YNKNgs
+
+## Tutorial Autenticacion:
+https://simpleisbetterthancomplex.com/tutorial/2018/11/22/how-to-implement-token-authentication-using-django-rest-framework.html
--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,13 @@
 from rest_framework import routers
+from django.urls import path
 from .api import ProjectViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 
 router.register('api/projects', ProjectViewSet, 'projects')
+# router.register('api/token-auth', obtain_auth_token, 'api_token_auth')
 
 urlpatterns = router.urls
+
+urlpatterns.append(path('api/token-auth', obtain_auth_token, name='api_token_auth'))
